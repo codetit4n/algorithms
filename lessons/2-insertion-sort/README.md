1be7b75b6ae3d126c2895b7ab8964f06063f38a1
codeTIT4N <lokeshkumar636@yahoo.com>
2023-06-27 23:41:59 +0530

    insertion sort

---
+++ lessons/2-insertion-sort/README.md
@@ -0,0 +1,5 @@
+# Insertion Sort
+
+Insertion sort works best on small number of elements.
+
+To execute the c++ code run: `sh execute`
---
+++ lessons/2-insertion-sort/insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int len) {
+  for (int i = 0; i < len; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+int main() {
+  int arr[] = {5, 2, 7, -1, 2, 0, 9, 1};
+  int len = sizeof(arr) / sizeof(int);
+  cout << "Input: ";
+  display(arr, len);
+
+  int i = 1;
+  while (i < len) {
+    int to_insert = arr[i];
+    int j = i - 1;
+    while (j >= 0 && to_insert < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = to_insert;
+    i++;
+  }
+
+  cout << endl;
+  cout << "Output: ";
+  display(arr, len);
+  return 0;
+}
